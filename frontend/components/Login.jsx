7c7832d8ab1c735bc410c4e97322501446360c27
Raj Sekar Dola <dola.rajsekar4@gmail.com>
2024-11-08 23:10:56 +0530

    Change in CSS

--- frontend/components/Login.jsx
+++ frontend/components/Login.jsx
@@ -1,62 +1,60 @@
 import React, { useState } from 'react';
-import {useNavigate } from 'react-router-dom';
-//import '../css/SignupLogin.css';
+import { useNavigate } from 'react-router-dom';
+import "../css/Login.css"
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [message,setmessage]=useState('');
-  const navigate=useNavigate();
+  const [message, setMessage] = useState('');
+  const navigate = useNavigate();
+
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response= await fetch('http://localhost:3000/login', 
+      const response = await fetch('http://localhost:3000/login', 
         { 
-          method:'POST',
-          headers:{'Content-Type':'application/json'},
-          body:JSON.stringify({
-            email:email,
-            password:password,
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            email: email,
+            password: password,
           }),
-          credentials:'include',
-        });
-        if(!response.ok)
-        {
-          const errorResponse = await response.json();
-          console.log(errorResponse.errormessage);
-          setError(true);
-          setmessage(errorResponse.errormessage || "An error occurred.");
+          credentials: 'include',
         }
-        else{
-          const data=await response.json();
-          if(data.user)
-          {sessionStorage.setItem('user_id',data.user._id);
-            setmessage('login successful');
-            setTimeout(() => {
-              navigate('/chatrooms');
-            }, 1000);
-          }
+      );
 
+      if (!response.ok) {
+        const errorResponse = await response.json();
+        setMessage(errorResponse.errormessage || "An error occurred.");
+      } else {
+        const data = await response.json();
+        if (data.user) {
+          sessionStorage.setItem('user_id', data.user._id);
+          setMessage('Login successful');
+          setTimeout(() => {
+            navigate('/chatrooms');
+          }, 1000);
         }
+      }
     } catch (err) {
       console.error(err);
     }
   };
 
   return (
-    <div id="signin-login-page">
-        <div className="auth-toggle">
-          <button onClick={() => navigate('/register')}>Sign up</button>
-          <button className="activebutton" onClick={() => navigate('/login')}>Sign in</button>
-        </div>
-    <div className="signup-login-container login-animate">
-    <h2 className="signup-login-title">Sign In</h2>
-    <form id='signupForm' onSubmit={handleLogin}>
-      <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
-      <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required/>
-      <button className='signuploginbutton' type="submit">Login</button>
-      <div>{message}</div>
-    </form>
-    </div>
+    <div className="login-page">
+      <div className="login-toggle">
+        <button onClick={() => navigate('/register')}>Sign up</button>
+        <button className="login-active-button" onClick={() => navigate('/login')}>Sign in</button>
+      </div>
+      <div className="login-container">
+        <h2 className="login-title">Sign In</h2>
+        <form className="login-form" onSubmit={handleLogin}>
+          <input className="login-input" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
+          <input className="login-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
+          <button className="login-submit-button" type="submit">Login</button>
+          <div className="login-message">{message}</div>
+        </form>
+      </div>
     </div>
   );
 };
