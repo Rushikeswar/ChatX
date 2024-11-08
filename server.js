93986b9e47dc364bcf10c91fcbd365dbbf6a448f
Rushikeswar <rushikesward@gmail.com>
2024-11-09 02:03:47 +0530

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL,
   credentials: true,
 }));
 app.use((err, req, res, next) => {
