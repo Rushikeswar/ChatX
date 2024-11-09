dcdfed2cd3f8bcd3edf39b945d15409b186ae4df
Rushikeswar <rushikesward@gmail.com>
2024-11-09 13:44:44 +0530

    mostly corrected sockets

--- frontend/components/Login.jsx
+++ frontend/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
     e.preventDefault();
     setMessage('');
     try {
-      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/login`, 
+      const response = await fetch(`https://chatx-vptn.onrender.com/login`, 
         { 
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
